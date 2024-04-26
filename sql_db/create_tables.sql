8526e4eae02858c9a39b3974f00e0ae72cdee3df
megict <cobraj@yandex.ru>
2024-04-26 23:37:04 +0300

    added all table creation and initializing with random data

--- sql_db/create_tables.sql
+++ sql_db/create_tables.sql
@@ -1,7 +1,7 @@
 
 CREATE TABLE public.users
 (
-    id bigint NOT NULL,
+    id serial NOT NULL,
     username text NOT NULL,
     pword_hash text NOT NULL,
     name_first text,
@@ -10,4 +10,47 @@ CREATE TABLE public.users
 );
 
 ALTER TABLE IF EXISTS public.users
+    OWNER to mxcitn;
+
+
+CREATE TABLE public.products
+(
+    id serial NOT NULL,
+    name text NOT NULL,
+    price bigint NOT NULL,
+    amount bigint NOT NULL,
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE IF EXISTS public.products
+    OWNER to mxcitn;
+
+    
+CREATE TABLE public.baskets
+(
+    id serial NOT NULL,
+    owner_user_id bigint NOT NULL,
+    time_opened timestamp NOT NULL,
+    time_colsed timestamp,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_user_id) REFERENCES public.users (id) ON DELETE CASCADE
+);
+
+ALTER TABLE IF EXISTS public.baskets
+    OWNER to mxcitn;
+
+
+
+
+CREATE TABLE public.basket_to_product
+(
+    id serial NOT NULL,
+    basket_id bigint NOT NULL,
+    product_id bigint NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (basket_id) REFERENCES public.baskets (id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES public.products (id) ON DELETE CASCADE
+);
+
+ALTER TABLE IF EXISTS public.basket_to_product
     OWNER to mxcitn;
---
+++ sql_db/initialize.py
@@ -0,0 +1,88 @@
+import psycopg2
+import random
+from random_word import RandomWords
+import names
+from datetime import datetime
+from datetime import timedelta
+import bcrypt
+from tqdm import tqdm
+
+salt = bcrypt.gensalt()
+rwg = RandomWords()
+
+PROD_CNT = 100 # сколько товаров добавить
+USER_CNT = 100 # сколько пользователей добавить
+
+
+def create_random_product():
+    name = rwg.get_random_word()
+    price = abs(int(random.gauss(mu = 200, sigma = 50)))
+    amount = abs(int(random.gauss(mu = 0, sigma = 10)))
+    return name, price, amount
+
+def create_random_basket():
+    opening_date = datetime.now() - timedelta(days = random.choice(range(1, 20)))
+    closing_date = random.choice([None, datetime.now()])
+
+    return opening_date, closing_date
+
+def create_random_user():
+    password = rwg.get_random_word()
+
+    user_name = rwg.get_random_word()
+    passwor_hash = bcrypt.hashpw(password.encode('utf-8'), salt).decode('utf-8')
+    
+    name_first = names.get_first_name()
+    name_last = names.get_last_name()
+
+    baskets = []
+    for _ in range(random.choice([1,1,1,1,2,2,3,4,5])):
+        baskets.append(create_random_basket())
+
+    return user_name, passwor_hash, name_first, name_last, baskets
+
+try:
+    # пытаемся подключиться к базе данных
+    conn = psycopg2.connect(dbname='shop_db', user='mxcitn', password='1234')
+except:
+    # в случае сбоя подключения будет выведено сообщение в STDOUT
+    print('Can`t establish connection to database')
+    conn = None
+
+# инициализация бд случайно сгенерированными данными о клиентах, товарах и корзинах
+if conn is not None:
+    cursor = conn.cursor()
+    random_user_data = create_random_user()
+    
+    for _ in tqdm(range(PROD_CNT)):
+        cursor.execute(f"INSERT INTO products (name, price, amount)\
+                            VALUES {create_random_product()};")
+        
+    cursor.execute("SELECT id FROM products")
+    avail_products = cursor.fetchall()
+
+    for _ in tqdm(range(USER_CNT)):
+        random_user_data = create_random_user()
+        cursor.execute(f"INSERT INTO users (username, pword_hash, name_first, name_last)\
+                            VALUES {random_user_data[0:4]};")
+        cursor.execute("SELECT id FROM users ORDER BY id desc limit 1")
+        # добавляем корзины для этого пользователя
+        user_id = cursor.fetchall()[0][0]
+        for basket in random_user_data[4]:
+            if basket[1] is None:
+                cursor.execute(f"INSERT INTO baskets (owner_user_id, time_opened)\
+                                    VALUES {user_id, str(basket[0])};")
+            else:
+                cursor.execute(f"INSERT INTO baskets (owner_user_id, time_opened, time_colsed)\
+                                    VALUES {user_id, str(basket[0]), str(basket[1])};")
+            cursor.execute("SELECT id FROM baskets ORDER BY id desc limit 1")
+            # наполняем корзину товарами
+            basket_id = cursor.fetchall()[0][0]
+            for _ in range(random.choice(range(10))):
+                cursor.execute(f"INSERT INTO basket_to_product (basket_id, product_id)\
+                                    VALUES {basket_id, random.choice(avail_products)[0]};")
+                
+
+    conn.commit()
+    cursor.close()
+    conn.close()
